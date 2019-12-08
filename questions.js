73944b043c3020e1c5b30b5d814c0c625640cfcf
Madison Schrews <maddyschrews@gmail.com>
2019-12-08 17:53:21 -0500

    updating existing files, making the html, css, and javascript work

---
+++ questions.js
@@ -0,0 +1,34 @@
+//The questions reflect the Star Wars canon franchise
+
+var questions = [
+    {
+        title: "What is the name of Han Solo's ship?",
+        choices: ["Millenium Falcon", "Millenial Falcon", "Kessel Run", "Baby Yoda"],
+        answer: "Millenium Falcon"
+    },
+    {
+        title: "What does Padme name her two children before her death?",
+        choices: ["Luke and Lynn", "David and Rebecca", "Cody and Mabel", "Luke and Leia"],
+        answer: "Luke and Leia"
+    },
+    {
+        title: "What bounty hunter captures Han Solo?",
+        choices: ["Jaba the Hutt", "The Mandalorian", "Boba Fett", "Yoda"],
+        answer: "Boba Fett", 
+    },
+    {
+        title: "What character is played by Adam Driver in Star Wars: The Last Jedi?",
+        choices: ["Kylo Ren", "Finn", "Rey", "Luke Skywalker"],
+        answer: "Kylo Ren"
+    },
+    {
+        title: "What is Darth Vader's birth name?",
+        choices: ["Anakin Skywalker", "Luke Skywalker", "Obi Wan Kenobi", "Ben Kenobi"],
+        answer: "Anakin Skywalker"
+    },
+
+
+];
+
+console.log(questions);
+
